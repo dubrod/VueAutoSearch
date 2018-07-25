4669e06d9b32eedb84d2be707cbce290be38298c
Wayne Roddy <dubrod@icloud.com>
2018-07-25 16:25:12 -0400

    Create Autocomplete.vue

---
+++ src/components/Autocomplete.vue
@@ -0,0 +1,12 @@
+<template>
+  <div class="auto-c-wrapper">
+      <input v-model="$root.acvalue" placeholder="Quick Search" type="text" />
+      <button v-if="$root.acvalue" v-on:click="$root.emptySearch()" class="auto-c-close">X</button>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'auto-complete'
+}
+</script>
